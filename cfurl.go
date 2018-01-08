8b3fb9bd8b88853cee1f5a71fd6e48bef0607519
Hiroyuki Sano <sh19910711@gmail.com>
2018-01-08 23:02:59 +0900

    extract problem statement, input and output

--- cfurl.go
+++ cfurl.go
@@ -14,16 +14,49 @@ func ExtractSourceCode(res *http.Response) string {
 	return strings.Trim(doc.Find(".program-source").Text(), "\n")
 }
 
+func ExtractProblemStatement(res *http.Response) string {
+	doc, _ := goquery.NewDocumentFromResponse(res)
+	return strings.Trim(doc.Find(".problem-statement").Text(), "\n")
+}
+
+func ExtractProblemInput(res *http.Response) string {
+	doc, _ := goquery.NewDocumentFromResponse(res)
+	return strings.Join(doc.Find(".sample-test .input pre").Map(
+		func(_ int, s *goquery.Selection) string {
+			return strings.Trim(s.Text(), "\n")
+		}), "\n")
+}
+
+func ExtractProblemOutput(res *http.Response) string {
+	doc, _ := goquery.NewDocumentFromResponse(res)
+	return strings.Join(doc.Find(".sample-test .output pre").Map(
+		func(_ int, s *goquery.Selection) string {
+			return strings.Trim(s.Text(), "\n")
+		}), "\n")
+}
+
 func main() {
+	input := flag.Bool("input", false, "")
+	output := flag.Bool("output", false, "")
 	flag.Parse()
 	if len(flag.Args()) == 0 {
 		println("ERR: no arguments")
 		os.Exit(-1)
 	}
+
 	u := flag.Args()[0]
 	res, _ := http.Get(u)
+
 	if strings.Contains(u, "submission") {
 		fmt.Print(ExtractSourceCode(res))
+	} else if strings.Contains(u, "/problem/") {
+		if *input {
+			fmt.Print(ExtractProblemInput(res))
+		} else if *output {
+			fmt.Print(ExtractProblemOutput(res))
+		} else {
+			fmt.Print(ExtractProblemStatement(res))
+		}
 	} else {
 		println("nothing to do :-|")
 	}
